b9300b0ca2cf017095056f65dca417c313d8754a
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2024-10-28 15:39:24 +0100

    Implement create publisher logic

--- src/modules/publishers/dto/create-publisher.dto.ts
+++ src/modules/publishers/dto/create-publisher.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePublisherDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePublisherDto {
+  @ApiProperty({
+    example: 'Publisher Name',
+    description: 'Name of the Publisher',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    example: 'Publisher Location',
+    description: 'Location of the Publisher',
+  })
+  @IsString()
+  @IsNotEmpty()
+  location: string;
+}
--- src/modules/publishers/publishers.service.ts
+++ src/modules/publishers/publishers.service.ts
@@ -1,11 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { CreatePublisherDto } from './dto/create-publisher.dto';
 import { UpdatePublisherDto } from './dto/update-publisher.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Publisher } from './entities/publisher.entity';
 
 @Injectable()
 export class PublishersService {
-  create(createPublisherDto: CreatePublisherDto) {
-    return 'This action adds a new publisher';
+  constructor(
+    @InjectRepository(Publisher)
+    private publisherRepository: Repository<Publisher>,
+  ) {}
+
+  async generateIdentifierCode(
+    createPublisherDto: CreatePublisherDto,
+  ): Promise<string> {
+    const { name, location } = createPublisherDto;
+    const formattedName = name.slice(0, 3).toUpperCase();
+    const formattedLocation = location
+      ? location.slice(0, 3).toUpperCase()
+      : 'AAA';
+    const randomNumber = Math.floor(100 + Math.random() * 900);
+    let identifierCode = `${formattedName}-${formattedLocation}-${randomNumber}`;
+    // Check if the identifierCode already exists in the database
+    const existingPublisher = await this.publisherRepository.findOne({
+      where: { identifierCode },
+    });
+    while (existingPublisher) {
+      // If it exists, generate a new identifierCode
+      identifierCode = `${formattedName}${formattedLocation}-${Math.floor(100 + Math.random() * 900)}`;
+    }
+    return identifierCode;
+  }
+  /**
+   * Create a new publisher
+   * @param createPublisherDto
+   * @returns savedPublisher
+   */
+  async createPublisher(
+    createPublisherDto: CreatePublisherDto,
+  ): Promise<Publisher> {
+    try {
+      const { name, location } = createPublisherDto;
+      const identifierCode =
+        await this.generateIdentifierCode(createPublisherDto);
+      const publisher = this.publisherRepository.create({
+        name,
+        location,
+        identifierCode,
+      });
+      const savedPublisher = await this.publisherRepository.save(publisher);
+      return savedPublisher;
+    } catch (error) {
+      console.error('Error creating publisher:', error.message);
+      throw new InternalServerErrorException(
+        'An error occurred creating publisher. Please check server logs for details.',
+        error.message,
+      );
+    }
   }
 
   findAll() {
